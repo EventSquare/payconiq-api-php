261574a2e7e009deb88664f4ec419770d2ecb634
Djairho Geuens <djairho.geuens@ae.be>
2020-06-24 22:32:15 +0200

    feat(#1): adds reference for createPayment

--- src/Client.php
+++ src/Client.php
@@ -3,27 +3,29 @@
 namespace Payconiq;
 
 use Payconiq\Support\Exceptions\CreateTransactionFailedException;
+use Payconiq\Support\Exceptions\CreatePaymentFailedException;
+use Payconiq\Support\Exceptions\GetPaymentDetailsFailedException;
 use Payconiq\Support\Exceptions\RetrieveTransactionFailedException;
 
 class Client
 {
-	
-	protected $merchant_id;
-	protected $access_token;
+
+	protected $merchantId;
+	protected $accessToken;
 	protected $endpoint = 'https://api.payconiq.com/v3';
-	
+
 	/**
 	 * Construct
 	 *
-	 * @param  string $merchent_id  The merchant ID registered with Payconiq.
-	 * @param  string $access_token  Used to secure request between merchant backend and Payconiq backend.
+	 * @param  string $merchantId  The merchant ID registered with Payconiq.
+	 * @param  string $accessToken  Used to secure request between merchant backend and Payconiq backend.
 	 * 
 	 * @return void
 	 */
-	public function __construct($merchant_id = null, $access_token = null)
+	public function __construct($merchantId = null, $accessToken = null)
 	{
-		$this->merchant_id = $merchant_id;
-		$this->access_token = $access_token;
+		$this->merchantId = $merchantId;
+		$this->accessToken = $accessToken;
 	}
 
 	/**
@@ -43,13 +45,13 @@ class Client
 	/**
 	 * Set the merchant id
 	 *
-	 * @param  string $merchent_id  The merchant ID registered with Payconiq.
+	 * @param  string $merchantId  The merchant ID registered with Payconiq.
 	 *
 	 * @return self
 	 */
-	public function setMerchantId($merchant_id)
+	public function setMerchantId($merchantId)
 	{
-		$this->merchant_id = $merchant_id;
+		$this->merchantId = $merchantId;
 
 		return $this;
 	}
@@ -57,13 +59,13 @@ class Client
 	/**
 	 * Set the access token
 	 *
-	 * @param  string $access_token  Used to secure request between merchant backend and Payconiq backend.
+	 * @param  string $accessToken  Used to secure request between merchant backend and Payconiq backend.
 	 *
 	 * @return self
 	 */
-	public function setAccessToken($access_token)
+	public function setAccessToken($accessToken)
 	{
-		$this->access_token = $access_token;
+		$this->accessToken = $accessToken;
 
 		return $this;
 	}
@@ -77,6 +79,9 @@ class Client
 	 * 
 	 * @return string  transaction_id
 	 * @throws CreateTransactionFailedException  If the response has no transactionid
+	 * 
+	 * @deprecated Use createPayment instead
+	 * @see createPayment
 	 */
 	public function createTransaction($amount, $currency, $callbackUrl)
 	{
@@ -86,25 +91,71 @@ class Client
 			'callbackUrl' => $callbackUrl,
 		]);
 
-		if(empty($response['transactionId']))
+		if (empty($response['transactionId']))
 			throw new CreateTransactionFailedException($response['message']);
 
 		return $response['transactionId'];
 	}
 
 	/**
-	 * Retrieve an existing transaction
+	 * Create a new payment
+	 * 
+	 * @param  float $amount  Payment amount in cents
+	 * @param  string $currency  Payment currency code in IOS 4217 format
+	 * @param  string $reference External payment reference used to reference the Payconiq payment in the calling party's system
+	 * @param  string $callbackUrl  A url to which the merchant or partner will be notified of a payment
+	 * 
+	 * @return string  paymentId
+	 * @throws CreatePaymentFailedException  If the response has no transactionid
+	 */
+	public function createPayment($amount, $currency = 'EUR', $reference, $callbackUrl)
+	{
+		$response = $this->curl('POST', $this->getEndpoint('/payments'), $this->constructHeaders(), [
+			'amount' => $amount,
+			'currency' => $currency,
+			'reference' => $reference,
+			'callbackUrl' => $callbackUrl,
+		]);
+
+		if (empty($response['paymentId']))
+			throw new CreatePaymentFailedException($response['message']);
+
+		return $response['paymentId'];
+	}
+
+	/**
+	 * Retrieve an existing payment
 	 *
-	 * @param  string $transaction_id  The transaction id provided by Payconiq
+	 * @param  string $transactionId  The transaction id provided by Payconiq
+	 *
+	 * @return  array  Response object by Payconiq
+	 * 
+	 * @deprecated Use getPaymentDetails instead
+	 * @see getPaymentDetails
+	 */
+	public function retrieveTransaction($transactionId)
+	{
+		$response = $this->curl('GET', $this->getEndpoint('/payments/' . $transactionId), $this->constructHeaders());
+
+		if (empty($response['paymentId']))
+			throw new RetrieveTransactionFailedException($response['message']);
+
+		return $response;
+	}
+
+	/**
+	 * Get payment details of an existing payment
+	 *
+	 * @param  string $paymentId  The unique Payconiq identifier of a payment as provided by the create payment service
 	 *
 	 * @return  array  Response object by Payconiq
 	 */
-	public function retrieveTransaction($transaction_id)
+	public function getPaymentDetails($paymentId)
 	{
-		$response = $this->curl('GET', $this->getEndpoint('/payments/'.$transaction_id), $this->constructHeaders());
+		$response = $this->curl('GET', $this->getEndpoint('/payments/' . $paymentId), $this->constructHeaders());
 
-		if(empty($response['paymentId']))
-			throw new RetrieveTransactionFailedException($response['message']);
+		if (empty($response['paymentId']))
+			throw new GetPaymentDetailsFailedException($response['message']);
 
 		return $response;
 	}
@@ -128,41 +179,40 @@ class Client
 	{
 		return [
 			'Content-Type: application/json',
-			'Authorization: '.$this->access_token,
+			'Authorization: ' . $this->accessToken,
 		];
 	}
-	
+
 	/**
-    * cURL request
-    *
-    * @param  string $method
-    * @param  string $url
-    * @param  array $headers
-    * @param  array $parameters
-    *
-    * @return response
-    */
-    private function cURL($method, $url, $headers=[], $parameters=[])
-    {
-        $curl = curl_init();
+	 * cURL request
+	 *
+	 * @param  string $method
+	 * @param  string $url
+	 * @param  array $headers
+	 * @param  array $parameters
+	 *
+	 * @return response
+	 */
+	private function cURL($method, $url, $headers = [], $parameters = [])
+	{
+		$curl = curl_init();
 
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_VERBOSE, 0);
-        curl_setopt($curl, CURLOPT_HEADER, 1);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,20);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 20);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($parameters));
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_VERBOSE, 0);
+		curl_setopt($curl, CURLOPT_HEADER, 1);
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 20);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 20);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($parameters));
 
-        $response = curl_exec($curl);
-        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        $body = substr($response, $header_size);
-        curl_close($curl);
+		$response = curl_exec($curl);
+		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+		$body = substr($response, $header_size);
+		curl_close($curl);
 
-        return json_decode($body,true);
-    }
-
+		return json_decode($body, true);
+	}
 }
--- src/Support/Exceptions/CreateTransactionFailedException.php
+++ src/Support/Exceptions/CreateTransactionFailedException.php
@@ -4,6 +4,11 @@ namespace Payconiq\Support\Exceptions;
 
 use Exception;
 
+/**
+ * @deprecated Use CreatePaymentFailedException instead
+ * 
+ * @see CreatePaymentFailedException
+ */
 class CreateTransactionFailedException extends Exception {
 
 	//
